e98b438da07f8762593c9bc4c3c60ecb86ff5560
tragicmilk7720 <drew.pranab7@gmail.com>
2022-11-21 10:48:56 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
 import requests
 
-base_url = "https://yfapi.net"
+url = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/AAPL,MSFT"
+
+headers = {
+	"X-RapidAPI-Key": "e12d9d9af3mshcb72c7f3893119bp129decjsn38df9d01169a",
+	"X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
+
