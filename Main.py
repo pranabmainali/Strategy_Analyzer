843ac09011499ba78050f66cfe28b545c83778c7
tragicmilk7720 <drew.pranab7@gmail.com>
2022-11-21 11:01:26 -0700

    cleared all files

--- Main.py
+++ Main.py
@@ -1,13 +1,1 @@
-import requests
-
-url = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/AAPL,MSFT"
-
-headers = {
-	"X-RapidAPI-Key": "e12d9d9af3mshcb72c7f3893119bp129decjsn38df9d01169a",
-	"X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
-}
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
 
