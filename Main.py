abfc4aefa3ff910a5ccc9fb10497154b4c3bd738
tragicmilk7720 <drew.pranab7@gmail.com>
2022-12-21 19:19:55 -0700

    added some new code

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
+import YFinance
 
+self 
+
+def getHistoricalData(ticker, interval):
+    link = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/hi/history/"+ticker+"/"+interval
+    print(YFinance.getRequests(link, api_key).text)
+
+getHistoricalData("MSFT", "1d")
--- YFinance.py
+++ YFinance.py
@@ -1,11 +1,6 @@
 import requests 
 
-api_key = input("Please input your personal rapid api key")
-
-def getHistoricalData(ticker, interval):
-    link = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/hi/history/"+ticker+"/"+interval.value
-    getRequests(link, api_key)
-
+api_key = "e12d9d9af3mshcb72c7f3893119bp129decjsn38df9d01169a"
 
 def getRequests(url, api_key):
     querystring = {"diffandsplits":"false"}
