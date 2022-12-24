aa6ebca1b16969fc1a710bdf98f451a2d85cdd76
tragicmilk7720 <drew.pranab7@gmail.com>
2022-12-23 17:01:15 -0700

    added code to turn historical data into json

--- Main.py
+++ Main.py
@@ -1,11 +1,25 @@
 import YFinance
+from stock_indicators.indicators.common.quote import Quote
+import json
 
 api_key = "e12d9d9af3mshcb72c7f3893119bp129decjsn38df9d01169a"
 
 def getHistoricalData(ticker, interval):
     link = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/hi/history/"+ticker+"/"+interval
-    print(YFinance.getRequests(link, api_key).text)
+    return YFinance.getRequests(link, api_key).text
 
-getHistoricalData("MSFT", "1d")
+#getHistoricalData("MSFT", "1d")
 
-#def setStock(ticker, interval):
+def get_historical_quotes(ticker, interval):
+    quotes = None
+
+    print(getHistoricalData(ticker, interval))
+
+    #returnData = json.loads(getHistoricalData(ticker, interval))
+    #print(returnData)
+    #for item in returnData['items']:
+    #    print(item)
+
+get_historical_quotes("MSFT", "1d")
+    
+
